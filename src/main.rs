b22c46632ecbc61424e17366d741ab40cbb8f0c5
Axel Magnuson <axelmagn@gmail.com>
2024-03-01 18:59:19 -0800

    Tilemap bugfixing (in progress)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use anyhow::Result;
 use macroquad::{
+    camera::{set_camera, Camera2D},
     color::{DARKGRAY, RED, WHITE},
+    file::load_string,
     input::{is_key_down, KeyCode},
+    math::Rect,
     shapes::draw_rectangle,
     text::draw_text,
+    texture::load_texture,
     time::get_frame_time,
     window::{clear_background, next_frame, screen_width},
 };
+use macroquad_tiled::{load_map, Map};
 
 struct Game {
     // tmp
@@ -15,12 +21,19 @@ struct Game {
     speed_min: f32,
     speed_max: f32,
     speed_sensitivity: f32,
+
+    // map
+    assets: Option<GameAssets>,
+
+    // camera
+    camera: Camera2D,
 }
 
 impl Game {
     fn new(screen_width: f32) -> Self {
         let rect_size = 120.;
         let position = (screen_width - rect_size) / 2.;
+
         Self {
             rect_size,
             position,
@@ -28,12 +41,45 @@ impl Game {
             speed_min: 100.,
             speed_max: 1000.,
             speed_sensitivity: 100.,
+
+            assets: None,
+
+            // TODO(axelmagn): fix magic numbers
+            camera: Camera2D::from_display_rect(Self::get_display_rect()),
         }
+    }
+
+    fn get_assets_ref(&self) -> &GameAssets {
+        &self.assets.as_ref().expect("assets not loaded")
+    }
+
+    fn get_display_rect() -> Rect {
+        Rect::new(0., 0., 1024., 786.)
     }
 }
 
 impl Lifecycle for Game {
-    fn handle_inputs(&mut self, delta_time: f32) {
+    async fn setup(&mut self) -> Result<()> {
+        let tile_texture =
+            load_texture("assets/kenney_tiny-dungeon/Tilemap/tilemap_packed.png").await?;
+        let tileset_json = load_string("assets/tiled/tiny_dungeon.tsj").await?;
+        let tile_map_json = load_string("assets/tiled/sandbox01.tmj").await?;
+
+        let tile_map = load_map(
+            &tile_map_json,
+            &[(
+                "../kenney_tiny-dungeon/Tilemap/tilemap_packed.png",
+                tile_texture,
+            )],
+            &[("tiny_dungeon.tsx", &tileset_json)],
+        )?;
+
+        self.assets = Some(GameAssets { tile_map });
+
+        Ok(())
+    }
+
+    async fn handle_inputs(&mut self, delta_time: f32) {
         let dt = delta_time;
 
         // update speed
@@ -55,30 +101,44 @@ impl Lifecycle for Game {
         self.position = self.position.clamp(0., screen_width() - self.rect_size);
     }
 
-    fn render(&mut self) {
+    async fn render(&mut self) {
         clear_background(DARKGRAY);
+        set_camera(&self.camera);
+
+        self.get_assets_ref()
+            .tile_map
+            .draw_tiles("terrain", Self::get_display_rect(), None);
+
         draw_text(&format!("Speed: {:8.02}", self.speed), 50., 50., 22., WHITE);
         draw_rectangle(self.position, 200.0, self.rect_size, self.rect_size, RED);
     }
 }
 
-trait Lifecycle {
-    fn handle_inputs(&mut self, delta_time: f32);
+struct GameAssets {
+    tile_map: Map,
+}
 
-    fn render(&mut self);
+trait Lifecycle {
+    async fn setup(&mut self) -> Result<()>;
+
+    async fn handle_inputs(&mut self, delta_time: f32);
+
+    async fn render(&mut self);
 }
 
 #[macroquad::main("Stonehold")]
 async fn main() {
     let mut game = Game::new(screen_width());
 
+    game.setup().await.expect("Game setup failed");
+
     loop {
         // abbreviate delta time
         let dt = get_frame_time();
 
-        game.handle_inputs(dt);
+        game.handle_inputs(dt).await;
 
-        game.render();
+        game.render().await;
 
         // wait for next frame
         next_frame().await
